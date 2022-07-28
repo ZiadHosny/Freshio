649f7cce4e301bdaef7d5bcd85c34a49f38e122d
khaled mohamed <khaledmohamed0140@gmail.com>
2022-07-29 01:37:26 +0200

    side bar

--- src/pages/auth/Account.jsx
+++ src/pages/auth/Account.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import "./account.css"
+import "./account.css";
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+
+
 
 
 const Account = () => {
   return  (
-    <h1>hello</h1>
+    <ProSidebar>
+  <Menu iconShape="square">
+    <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
+    <SubMenu title="Components" icon={<FaHeart />}>
+      <MenuItem>Component 1</MenuItem>
+      <MenuItem>Component 2</MenuItem>
+    </SubMenu>
+  </Menu>
+</ProSidebar>
   )
 };
 
