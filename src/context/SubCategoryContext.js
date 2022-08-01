38b36ba482bc90c2e71bc56192f4879f24259402
Ziad Hosny <ziadhosny007#gmail.com>
2022-08-01 22:09:53 +0200

    update link low-carb

--- src/context/SubCategoryContext.js
+++ src/context/SubCategoryContext.js
@@ -1,50 +1,39 @@
-import React from 'react'
-import { createContext, useState} from "react";
-import axios from "axios";
+import React from 'react';
+import { createContext, useState } from 'react';
+import axios from 'axios';
 
 export let subApiContext = createContext([]);
 
 export default function SubCategoryContext(props) {
- 
   const [subCategory, setSubCategory] = useState(null);
 
   let allCategory = [];
   let allProduct = [];
-  
 
   async function getSubData(path) {
-
-    if (path == "/") {
-      
+    if (path == '/') {
       let { data } = await axios.get(
-
-        `https://freshfreshio.mocklab.io/home`
-
+        `https://healthy-food-ed8b5-default-rtdb.firebaseio.com/home.json`
       );
-     setSubCategory(data);
-    }
-    else {
-
+      setSubCategory(data);
+    } else {
       let { data } = await axios.get(
-        `https://foodfresh.mocklab.io${path}`
+        `https://healthy-food-ed8b5-default-rtdb.firebaseio.com/${path}.json`
       );
 
       allCategory = Object.values(data);
-      allCategory.forEach((item)=>{
-        allProduct.push(...item)
-       })
+      allCategory.forEach((item) => {
+        allProduct.push(...item);
+      });
 
-       setSubCategory(allProduct);
-       console.log(allProduct)
-     
+      setSubCategory(allProduct);
+      console.log(allProduct);
     }
-
   }
 
-
   return (
-    <subApiContext.Provider value={{ subCategory ,getSubData }}>{props.children}</subApiContext.Provider>
-  )
+    <subApiContext.Provider value={{ subCategory, getSubData }}>
+      {props.children}
+    </subApiContext.Provider>
+  );
 }
-
-
