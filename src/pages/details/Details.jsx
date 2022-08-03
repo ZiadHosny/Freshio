aad8010f63a0c1fdb22d34aa8d3517511b6ecd66
Ziad Hosny <ziadhosny007#gmail.com>
2022-08-03 16:55:49 +0200

    update details

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,9 +3,15 @@ import './Details.css';
 import { BsHeart, BsCart } from 'react-icons/bs';
 import { DetailsContext } from '../../context/DetailsContext';
 import { Container } from '@mui/material';
+import { CartContext } from '../../context/CartContext';
 
 const Details = ({ onClick }) => {
   let { item } = useContext(DetailsContext);
+  const { addToCartInFirebase } = useContext(CartContext);
+
+  const addToCart = () => {
+    addToCartInFirebase(item);
+  };
 
   const truncateString = (str, num) => {
     if (str.length > num) {
@@ -18,24 +24,63 @@ const Details = ({ onClick }) => {
   return (
     <Container className="ContainerModal" onClick={onClick}>
       <div className="row border shadow  rounded p-2 h-100">
-        <div className="col-md-4">
-          <img src={item.image} className="w-100 " alt="/" />
+        <div className="col-md-4 h-100">
+          <img src={item.image} className="w-100 h-50" alt="/" />
+          <img src={item.nutrition} className="w-100 h-50" alt={item.title} />
         </div>
 
         <div className="col-md-7 ml-5  offset-md-1 col-12">
-          <p className="h2 fw-bold">{item.title}</p>
-          <p className="mt-3">{item.price} EGP</p>
-          <button className="btn btn-outline-success addBtn px-5 my-3 ">
-            Add
-          </button>
+          <p className="h2 fw-bold mb-4">{item.title}</p>
+          <div className="d-flex justify-content-between">
+            {item.sale ? (
+              <div className="d-flex">
+                <h5 className="text-decoration-line-through ">
+                  {item.price} EGP
+                </h5>
+                <h5 className="mx-2 text-color">
+                  {Math.floor(item.price * 0.8)} EGP
+                </h5>
+              </div>
+            ) : (
+              <h5>{item.price} EGP</h5>
+            )}
+            {item.calories ? (
+              <h5>
+                <span className="text-color"> {item.calories}</span> Calories
+              </h5>
+            ) : null}
+          </div>
+
           <div className="mt-3">
             <BsHeart className=" fs-3" />
-            <span className="ps-2 fs-20 link">add to wishlist</span>
-            <BsCart className="ms-2 fs-3" />
-            <span className="ps-2 link">add to cart</span>
+            <span className="ps-2 fs-20 link">Add To Favorites</span>
           </div>
-          <p className="h5 mt-4 fw-bold">Description</p>
-          <p>{truncateString(item.body, 360)}</p>
+          {item.ProductData ? (
+            <>
+              <p className="h5 mt-4 fw-bold">Product Data</p>
+              <div className="row">
+                <p className="col-6">Flavor : {item.ProductData.Flavor}</p>
+                <p className="col-6">Company : {item.ProductData.company}</p>
+                <p className="col-6">
+                  Formation : {item.ProductData.formation}
+                </p>
+                <p className="col-6">Serving : {item.ProductData.serving}</p>
+                <p className="col-6">Weight : {item.ProductData.weight}</p>
+              </div>
+            </>
+          ) : (
+            <>
+              <p className="h5 mt-4 fw-bold">Description</p>
+              <p>{truncateString(item.body, 360)}</p>
+            </>
+          )}
+
+          <button
+            className="btn btn-outline-success addBtn px-3 my-3  font-weight-bold"
+            onClick={addToCart}
+          >
+            Add To Cart <BsCart className="ms-2 fs-4" />
+          </button>
         </div>
       </div>
     </Container>
