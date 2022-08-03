1f66ac2e82e4ec7a190dfa2632161dec3993e389
Ziad Hosny <ziadhosny007#gmail.com>
2022-08-04 00:46:55 +0200

    adding account context

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { IoMdArrowRoundBack } from 'react-icons/io';
-import notFound from "../assets/232.jpg"
-
+import notFound from '../assets/232.jpg';
 
 export const NotFound = () => {
-    let navigate=useNavigate()
-    const navigateHome= ()=>{
-        navigate("/")
-    }
-    return (
-        <div className='container erourContainer '>
-            <button className="btn btn-outline-success addBtn px-2 my-3 " onClick={navigateHome}>
-                <span className='px-2'>
-                <IoMdArrowRoundBack/>
-                </span>
-                Back to home
-            </button>
-            <img src={notFound} className="w-100 "/>
-
-        </div>
-    )
-}
-export default NotFound
+  let navigate = useNavigate();
+  const navigateHome = () => {
+    navigate('/');
+  };
+  return (
+    <div className="container erourContainer ">
+      <button
+        className="btn btn-outline-success addBtn px-2 my-3 "
+        onClick={navigateHome}
+      >
+        <span className="px-2">
+          <IoMdArrowRoundBack />
+        </span>
+        Back to home
+      </button>
+      <img src={notFound} className="w-100 " />
+    </div>
+  );
+};
+export default NotFound;
