fb5bba03fc436d545b583b3c10f083b257586774
AhMEedMOStfa <ch.ahmedmostfa@gmail.com>
2022-08-04 15:26:43 +0200

    fix error className HtmlFor

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -23,27 +23,27 @@ const Contact = () => {
       }
   return (
     <div className="mt-5 container-lg">
-        <div class="text-center">
-            <h3 class="text-success">How Can We Help You?</h3>
+        <div className="text-center">
+            <h3 className="text-success">How Can We Help You?</h3>
         </div>
         <div className="row d-flex">
             <div className="bg-white col-12 col-md-6 align-items-center ">
                 <form onSubmit={handleSubmit} className="p-4 rounded shadow-md">
                     <h1 className="my-4 fw-bolder text-center text-md-start">Contact Us</h1>
                     <div>
-                        <label for="name" className="form-label">Your Name</label>
+                        <label htmlFor="name" className="form-label">Your Name</label>
                         <input onChange={getData} type="text" name="name" className="form-control" placeholder="Your Name" required/>
                     </div>
                     <div className="mt-3">
-                        <label for="email" className="form-label">Your Email</label>
+                        <label htmlFor="email" className="form-label">Your Email</label>
                         <input onChange={getData} type="text" name="email" className="form-control" placeholder="Your Email" required/>
                     </div>
                     <div className="mt-3">
-                        <label for="subject" className="form-label">Subject</label>
+                        <label htmlFor="subject" className="form-label">Subject</label>
                         <input onChange={getData} type="text" name="subject" className="form-control" placeholder="Subject" required/>
                     </div>
                     <div className="mt-3 mb-3">
-                        <label for="message" className="form-label">Message</label>
+                        <label htmlFor="message" className="form-label">Message</label>
                         <textarea onChange={getData} name="message" cols="20" rows="6" className="form-control"
                             placeholder="message"></textarea>
                     </div>
