09a8a85dbf8883d7fba063557470e5190f979927
Ziad Hosny <ziadhosny007#gmail.com>
2022-08-09 13:50:10 +0200

    'finish'

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
           <FavoritesProvider>
             <Navbar />
             <Modal />
-            {/* <Chat /> */}
+            <Chat />
             <ScrollArrow />
             <Routes>
               <Route path="/" element={<Home />} />
